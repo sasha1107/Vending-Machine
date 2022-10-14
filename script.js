52c7caf39af67a3defa5a3a6140556859ec4b42e
Soohyun Jung <sasha1107@naver.com>
2022-10-14 23:28:14 +0900

    Refactor: 데이터를 json 형식으로 수정

--- script.js
+++ script.js
@@ -1,62 +1,56 @@
-let drinks = {
-    // key(키) : {
-    //     name(이름):
-    //     img(이미지 경로):
-    //     price(가격):
-    //     count(선택한 음료 개수):
-    //     stock(재고):
-    // }
-    original: {
-        name: "Original_Cola",
-        img: "./mediaquery/Original_Cola.png",
-        price: 1000,
-        count: 0,
-        stock: 3,
+let drinks = [
+    {
+        "name": "Original_Cola",
+        "img": "./mediaquery/Original_Cola.png",
+        "price": 1000,
+        "count": 0,
+        "stock": 3
     },
-    violet: {
-        name: "Violet_Cola",
-        img: "./mediaquery/Violet_Cola.png",
-        price: 1000,
-        count: 0,
-        stock: 3,
+    {
+        "name": "Violet_Cola",
+        "img": "./mediaquery/Violet_Cola.png",
+        "price": 1000,
+        "count": 0,
+        "stock": 3
     },
-    yellow: {
-        name: "Yellow_Cola",
-        img: "./mediaquery/Yellow_Cola.png",
-        price: 1000,
-        count: 0,
-        stock: 3,
+    {
+        "name": "Yellow_Cola",
+        "img": "./mediaquery/Yellow_Cola.png",
+        "price": 1000,
+        "count": 0,
+        "stock": 3
     },
-    cool: {
-        name: "Cool_Cola",
-        img: "./mediaquery/Cool_Cola.png",
-        price: 1000,
-        count: 0,
-        stock: 3,
+    {
+        "name": "Cool_Cola",
+        "img": "./mediaquery/Cool_Cola.png",
+        "price": 1000,
+        "count": 0,
+        "stock": 3
     },
-    green: {
-        name: "Green_Cola",
-        img: "./mediaquery/Green_Cola.png",
-        price: 1000,
-        count: 0,
-        stock: 3,
+    {
+        "name": "Green_Cola",
+        "img": "./mediaquery/Green_Cola.png",
+        "price": 1000,
+        "count": 0,
+        "stock": 3
     },
-    orange: {
-        name: "Orange_Cola",
-        img: "./mediaquery/Orange_Cola.png",
-        price: 1000,
-        count: 0,
-        stock: 3,
-    },
-};
+    {
+        "name": "Orange_Cola",
+        "img": "./mediaquery/Orange_Cola.png",
+        "price": 1000,
+        "count": 0,
+        "stock": 3
+    }
+];
 let walletCoin = 25000; //소지금
 let inputCoin = 10000; // 입금액
 let totalPrice = 0; //총 금액 (총금엑 += 음료가격 * 음료 개수)
-let itemnum = Object.keys(drinks).length;
-for (let i = 0; i < itemnum; i++) {
+
+let itemNum = drinks.length;
+for (let i = 0; i < itemNum; i++) {
     totalPrice +=
-        parseInt(Object.values(drinks)[i].price) *
-        parseInt(Object.values(drinks)[i].count);
+        parseInt(drinks[i].price) *
+        parseInt(drinks[i].count);
 }
 let balance = inputCoin - totalPrice; //잔액
 
@@ -102,33 +96,33 @@ function changeBalance() {
 // 음료 클릭 시 선택 count를 늘리는 함수
 function selectItem(id) { 
     //재고가 없을 땐 선택할 수 없도록 예외처리
-    if (Object.values(drinks)[id].stock < 1) {
+    if (drinks[id].stock < 1) {
         alert("재고가 없습니다!")
         return;
     }
-    Object.values(drinks)[id].count += 1;
+    drinks[id].count += 1;
 
-    if (Object.values(drinks)[id].count > 1) {
+    if (drinks[id].count > 1) {
         // 이미 선택되어 있다면 새로 생성하지 않고 카운트만 증가
-        document.getElementById(Object.values(drinks)[id].name).innerHTML = Object.values(drinks)[id].count;
-        Object.values(drinks)[id].stock -= 1;
+        document.getElementById(drinks[id].name).innerHTML = drinks[id].count;
+        drinks[id].stock -= 1;
 
         // 재고(stock)가 0이 되면 품절 마크를 표시
         item_list = document.getElementsByClassName("item-li");
-        for (let i = 0; i < itemnum; i++) {
-            if (Object.values(drinks)[i].stock == 0) {
+        for (let i = 0; i < itemNum; i++) {
+            if (drinks[i].stock == 0) {
                 item_list[i].classList.add("soldout");
             }
         }
     }
     else {
         addtoSelectedList(id);
-        Object.values(drinks)[id].stock -= 1;
+        drinks[id].stock -= 1;
 
         // 재고(stock)가 0이 되면 품절 마크를 표시
         item_list = document.getElementsByClassName("item-li");
-        for (let i = 0; i < itemnum; i++) {
-            if (Object.values(drinks)[i].stock == 0) {
+        for (let i = 0; i < itemNum; i++) {
+            if (drinks[i].stock == 0) {
                 item_list[i].classList.add("soldout");
             }
         }
@@ -157,28 +151,28 @@ function addtoSelectedList(id) {
     const span2Tag = document.createElement("span");
     liTag.setAttribute("class", "get-drinks");
     pTag.setAttribute("class", "get-drinks-item");
-    imgTag.setAttribute("src", Object.values(drinks)[id].img);
-    imgTag.setAttribute("alt", Object.values(drinks)[id].name);
+    imgTag.setAttribute("src", drinks[id].img);
+    imgTag.setAttribute("alt", drinks[id].name);
     span1Tag.setAttribute("class", "get-drinks-item-name");
     span2Tag.setAttribute("class", "count");
-    span2Tag.setAttribute("id", Object.values(drinks)[id].name);
+    span2Tag.setAttribute("id", drinks[id].name);
     
     tagArea.appendChild(liTag);
     liTag.appendChild(pTag);
     pTag.appendChild(imgTag);
     pTag.appendChild(span1Tag);
     liTag.appendChild(span2Tag);
-    span1Tag.innerHTML = Object.values(drinks)[id].name;
-    span2Tag.innerHTML = Object.values(drinks)[id].count;
+    span1Tag.innerHTML = drinks[id].name;
+    span2Tag.innerHTML = drinks[id].count;
 }
 
 function getDrink() {
     let currentPrice = null;
     // let totalPrice = t; // 총 금액 (총금엑 += 음료가격 * 음료 개수)
-    for (let i = 0; i < itemnum; i++) {
+    for (let i = 0; i < itemNum; i++) {
         currentPrice +=
-            parseInt(Object.values(drinks)[i].price) *
-            parseInt(Object.values(drinks)[i].count);
+            parseInt(drinks[i].price) *
+            parseInt(drinks[i].count);
     }
 
     // 잔액보다 금액이 많으면 획득할 수 없게 하고
@@ -193,8 +187,8 @@ function getDrink() {
     balance.toLocaleString() + "원";
 
     
-    for (let i = 0; i < itemnum; i++) {
-        if (Object.values(drinks)[i].count > 0) {
+    for (let i = 0; i < itemNum; i++) {
+        if (drinks[i].count > 0) {
             // 획득한 음료로 넘기기
             let getArea = document.getElementById("getarea");
             //      <li class="get-drinks">
@@ -215,8 +209,8 @@ function getDrink() {
             const span2Tag = document.createElement("span");
             liTag.setAttribute("class", "get-drinks");
             pTag.setAttribute("class", "get-drinks-item");
-            imgTag.setAttribute("src", Object.values(drinks)[i].img);
-            imgTag.setAttribute("alt", Object.values(drinks)[i].name);
+            imgTag.setAttribute("src", drinks[i].img);
+            imgTag.setAttribute("alt", drinks[i].name);
             span1Tag.setAttribute("class", "get-drinks-item-name");
             span2Tag.setAttribute("class", "count");
             
@@ -225,8 +219,8 @@ function getDrink() {
             pTag.appendChild(imgTag);
             pTag.appendChild(span1Tag);
             liTag.appendChild(span2Tag);
-            span1Tag.innerHTML = Object.values(drinks)[i].name;
-            span2Tag.innerHTML = Object.values(drinks)[i].count;
+            span1Tag.innerHTML = drinks[i].name;
+            span2Tag.innerHTML = drinks[i].count;
         }
     }
     
@@ -236,8 +230,8 @@ function getDrink() {
     parent.innerHTML = "";
 
     // count = 0으로 초기화
-    for (let i = 0; i < itemnum; i++) {
-            Object.values(drinks)[i].count = 0;
+    for (let i = 0; i < itemNum; i++) {
+            drinks[i].count = 0;
     }
     totalPrice += currentPrice;
     currentPrice = 0;
@@ -250,10 +244,10 @@ function getDrink() {
 drink_list = document.getElementsByClassName("items-img");
 pricetag_list = document.getElementsByClassName("items-pricetag");
 drinks_name = document.getElementsByClassName("items-name");
-for (let i = 0; i < itemnum; i++) {
-    let src = Object.values(drinks)[i].img;
-    let price = Object.values(drinks)[i].price;
-    let name = Object.values(drinks)[i].name;
+for (let i = 0; i < itemNum; i++) {
+    let src = drinks[i].img;
+    let price = drinks[i].price;
+    let name = drinks[i].name;
     drink_list[i].src = src;
     drink_list[i].alt = name;
     drinks_name[i].innerHTML = name;
@@ -269,8 +263,8 @@ for (let i = 0; i < itemnum; i++) {
 // }
 
 // 잔액을 찍어주는 코드
-// document.getElementById("balance_result").innerHTML =
-//     balance.toLocaleString() + "원";
+document.getElementById("balance_result").innerHTML =
+    balance.toLocaleString() + "원";
 
 //소지금을 찍어주는 코드
 document.getElementById("wallet_coin").innerHTML =
